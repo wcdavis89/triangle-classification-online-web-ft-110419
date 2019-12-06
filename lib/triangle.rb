766a8326b0543e2bd47896632669b8db5d0b1e30
Whittney Davis <wcdavis89@yahoo.com>
2019-12-06 05:32:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,7 +25,10 @@ class Triangle
   def valid?
     if (sides.each {|side| side == 0})
       raise TriangleError
-      elsif 
+      elsif side1 + side2 <side3 && side2 + side3 <side1 && side1 + side3 <side2
+      raise TriangleError
+    end
+  end
     }
     
   
