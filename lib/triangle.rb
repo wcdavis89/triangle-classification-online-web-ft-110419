ebcad251e711a5cbb18b9e18cc0e6d6970e42b90
Whittney Davis <wcdavis89@yahoo.com>
2019-12-06 05:09:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  
+  @@sides=[]
+  
+  def initialize
+    @side1=side1
+    @side2=side2
+    @side3=side3
+    @@sides << side1
+    @@sides << side2
+    @@sides << side3
+  end
+  
+  def self.sides 
+    @@sides 
+  end
+  
+  
+  def valid?
+    sides.each {|side| }
+    
 end
